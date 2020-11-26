da782bbac848e5be46b38dd75999dce7783cef02
Wesley Adriano Pinheiro <pinheirocfc@gmail.com>
2020-11-26 11:11:22 -0300

    Update Send_WhatsWeb.py

--- Send_WhatsWeb.py
+++ Send_WhatsWeb.py
@@ -1,6 +1,8 @@
 #pinheirocfc@gmail.com
 #https://github.com/Wesley-Pinheiro
+#instrucoes em: https://youtu.be/78NoGpfiPpk
 
+#bibliotecas necessarias, caso nao tenha instalada em sua maquina basta executar os comandos (pip instal....)
 from selenium import webdriver  #pip install selenium
 import time
 from webdriver_manager.chrome import ChromeDriverManager #pip install webdriver_manager
@@ -9,19 +11,17 @@ from selenium.webdriver.common.keys import Keys
 #Abre o Chrome
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://web.whatsapp.com/') #abre o site Whatsapp Web
-time.sleep(15) #da um time-out de 15 segundos, tempo para scannear o codigo de barras
+time.sleep(15) #da um sleep de 15 segundos, tempo para scannear o codigo de barras
 
-#Contatos/Grupos - Informar o nome(s) de Grupos ou contatos que serao enviadas as mensagens
+#Contatos/Grupos - Informar o nome(s) de Grupos ou Contatos que serao enviadas as mensagens
 contatos = ['PALMEIRAS FUT','CONDOMINIO AREZZO','FAMILIA 02','FUTEBOL 2020','CLIENTES 02']
 
 #Mensagem - Mensagem que sera enviada
 mensagem = 'Bom dia grupo '
 mensagem2 = ' ,que o dia de voces seja iluminado'
 
-#Midia = imagem, pdf, documento, video
+#Midia = imagem, pdf, documento, video (caminho do arquivo, lembrando que mesmo no windows o caminho deve ser passado com barra invertida */* ) 
 midia = "/home/pinheirocfc/Imagens/bom-dia.jpg"
-
-
 
 #Funcao que pesquisa o Contato/Grupo
 def buscar_contato(contato):
@@ -47,8 +47,6 @@ def enviar_midia(midia):
     time.sleep(3)
     send = driver.find_element_by_xpath("//div[contains(@class, '_3Git-')]")
     send.click()    
-
-
 
 #Percorre todos os contatos/Grupos e envia as mensagens
 for contato in contatos:
